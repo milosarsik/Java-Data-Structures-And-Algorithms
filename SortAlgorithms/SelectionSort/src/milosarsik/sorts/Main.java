0ee9b3fdc34817d09d83d6e2c6aeb39e2ea855d2
milosarsik <milosarsic14@gmail.com>
2018-12-21 14:27:41 -0500

    Added SelectionSort

--- SortAlgorithms/SelectionSort/src/milosarsik/sorts/Main.java
+++ SortAlgorithms/SelectionSort/src/milosarsik/sorts/Main.java
@@ -4,7 +4,42 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        System.out.println("Selection Sort");
 
+        int[] integerArray = {3,4,23,34,235,23,14,35,234};
+
+        // Selection sort beings, we are keeping track of the end of the sorted partition
+        for(int endOfUnsortedPartition = integerArray.length - 1; endOfUnsortedPartition > 0; endOfUnsortedPartition--)
+        {
+            int indexOfLargest = 0;                                         // Storing the largest index
+
+            for(int i = 1; i <= endOfUnsortedPartition; i++)                // Traversing the array left to right
+            {
+                if(integerArray[i] > integerArray[indexOfLargest])          // Comparing elements at each index with the largest element
+                {
+                    indexOfLargest = i;
+                }
+            }
+
+            swap(integerArray,indexOfLargest,endOfUnsortedPartition);       // Calling swap
+        }
+
+        for (int i = 0; i < integerArray.length; i++)
+        {
+            System.out.println(integerArray[i]);                            // Printing the element
+        }
+    }
+
+    // swap - method that will swap elements at specified indices
+    public static void swap(int[] tempArray, int i, int j)
+    {
+        if(i == j)
+        {
+            return;
+        }
+
+        int temp = tempArray[i];
+        tempArray[i] = tempArray[j];
+        tempArray[j] = temp;
     }
 }
