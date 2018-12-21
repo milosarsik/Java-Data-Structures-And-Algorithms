223504100761f58330bf33023de2caba9d7998b7
milosarsik <milosarsic14@gmail.com>
2018-12-21 14:02:58 -0500

    Updated

--- SortAlgorithms/BubbleSort/src/milosarsik/sorts/Main.java
+++ SortAlgorithms/BubbleSort/src/milosarsik/sorts/Main.java
@@ -4,7 +4,43 @@ public class Main
 {
     public static void main(String[] args)
     {
+        System.out.println("Bubble Sort");
 
+        int[] integerArray = {6,2,7,8,3,2,7,45,3465,435};
 
+        for (int i = 0; i < integerArray.length; i++)
+        {
+            System.out.println(integerArray[i]);            // Printing the element
+        }
+
+        // Bubble sort begins, we are keeping track of the end of the unsorted partition
+        for (int endOfUnsortedPartition = integerArray.length - 1; endOfUnsortedPartition > 0; endOfUnsortedPartition--)
+        {
+            for (int i = 0; i < endOfUnsortedPartition; i++)    // In this loop we are traversing right to left
+            {
+                if(integerArray[i] > integerArray[i+1])
+                {
+                    swap(integerArray, i, i+1);         // Calling swap if true
+                }
+            }
+        }
+
+        for (int i = 0; i < integerArray.length; i++)
+        {
+            System.out.println(integerArray[i]);            // Printing the element
+        }
+    }
+
+    // swap - method that will swap elements at specified indices
+    public static void swap(int[] tempArray, int i, int j)
+    {
+        if(i == j)
+        {
+            return;
+        }
+
+        int temp = tempArray[i];
+        tempArray[i] = tempArray[j];
+        tempArray[j] = temp;
     }
 }
---
+++ SortAlgorithms/SelectionSort/src/milosarsik/sorts/Main.java
@@ -0,0 +1,10 @@
+package milosarsik.sorts;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        
+
+    }
+}
