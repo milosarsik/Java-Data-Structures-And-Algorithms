e2c394657cea9a6740112cbc8f44d6357caa495b
Milos Arsik <44819768+milosarsik@users.noreply.github.com>
2018-12-24 13:04:09 -0500

    Update Main.java

--- Queues/ArrayQueue/src/milosarsik/queues/Main.java
+++ Queues/ArrayQueue/src/milosarsik/queues/Main.java
@@ -11,25 +11,29 @@ public class Main
         Employee billEnd = new Employee("Bill", "End", 78);
 
         ArrayQueue queue = new ArrayQueue(10);
+        
         queue.add(janeJones);
         queue.add(johnDoe);
         queue.add(marySmith);
         queue.add(mikeWilson);
         queue.add(billEnd);
+        
         //queue.printQueue();
 
         queue.remove();
         queue.remove();
+        
         //queue.printQueue();
 
         //System.out.println(queue.peek());
+        
         queue.remove();
         queue.remove();
         queue.remove();
+        
         //queue.remove();
 
         queue.add(mikeWilson);
-
 
         queue.printQueue();
     }
