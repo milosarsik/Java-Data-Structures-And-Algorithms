560206781f34a35e0ea0ea42a54f82dc9d9537c3
Milos Arsik <44819768+milosarsik@users.noreply.github.com>
2018-12-24 13:06:39 -0500

    Update ArrayQueue.java

--- Queues/CircularQueue/src/milosarsik/queues/ArrayQueue.java
+++ Queues/CircularQueue/src/milosarsik/queues/ArrayQueue.java
@@ -2,18 +2,21 @@ package milosarsik.queues;
 
 import java.util.NoSuchElementException;
 
-public class ArrayQueue {
-
+public class ArrayQueue 
+{
     private Employee[] queue;
     private int front;
     private int back;
 
-    public ArrayQueue(int capacity) {
+    public ArrayQueue(int capacity) 
+    {
         queue = new Employee[capacity];
     }
 
-    public void add(Employee employee) {
-        if (size() == queue.length - 1) {
+    public void add(Employee employee) 
+    {
+        if (size() == queue.length - 1) 
+        {
             int numItems = size();
             Employee[] newArray = new Employee[2 * queue.length];
 
@@ -40,67 +43,84 @@ public class ArrayQueue {
         // 5
         // 9
 
-
         queue[back] = employee;
-        if (back < queue.length - 1) {
+        
+        if (back < queue.length - 1) 
+        {
             back++;
         }
-        else {
+        else 
+        {
             back = 0;
         }
     }
 
-    public Employee remove() {
-        if (size() == 0) {
+    public Employee remove() 
+    {
+        if (size() == 0) 
+        {
             throw new NoSuchElementException();
         }
 
         Employee employee = queue[front];
+        
         queue[front] = null;
         front++;
-        if (size() == 0) {
+        
+        if (size() == 0) 
+        {
             front = 0;
             back = 0;
         }
-        else if (front == queue.length) {
+        else if (front == queue.length) 
+        {
             front = 0;
         }
 
         return employee;
     }
 
-    public Employee peek() {
-        if (size() == 0) {
+    public Employee peek() 
+    {
+        if (size() == 0) 
+        {
             throw new NoSuchElementException();
         }
 
         return queue[front];
     }
 
-    public int size() {
-        if (front <= back) {
+    public int size() 
+    {
+        if (front <= back) 
+        {
             return back - front;
         }
-        else {
+        else 
+        {
             return back - front + queue.length;
         }
-
     }
 
-    public void printQueue() {
-        if (front <= back) {
-            for (int i = front; i < back; i++) {
+    public void printQueue()
+    {
+        if (front <= back) 
+        {
+            for (int i = front; i < back; i++) 
+            {
                 System.out.println(queue[i]);
             }
         }
-        else {
-            for (int i = front; i < queue.length; i++) {
+        else 
+        {
+            for (int i = front; i < queue.length; i++) 
+            {
                 System.out.println(queue[i]);
             }
-            for (int i = 0; i < back; i++) {
+            for (int i = 0; i < back; i++) 
+            {
                 System.out.println(queue[i]);
             }
         }
     }
-
 }
