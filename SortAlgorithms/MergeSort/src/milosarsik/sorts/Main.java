c3cdab1fb564233c946af4c40f8f5acfa9a23440
Milos Arsik <44819768+milosarsik@users.noreply.github.com>
2019-01-16 10:12:16 -0500

    Update Main.java
    Added the Merge Sort implementation we did in class (yeah... don't use it).

--- SortAlgorithms/MergeSort/src/milosarsik/sorts/Main.java
+++ SortAlgorithms/MergeSort/src/milosarsik/sorts/Main.java
@@ -26,8 +26,8 @@ public class Main
 
         int mid = (start + end) / 2;
 
-        mergeSort(input, start, mid);
-        mergeSort(input, mid, end);
+        mergeSort(input, start, mid);                       // Splitting the array
+        mergeSort(input, mid, end);                         // Splitting the array
 
         merge(input, start, mid, end);
     }
@@ -53,5 +53,71 @@ public class Main
 
         System.arraycopy(input, i, input, start + tempIndex, mid - i);
         System.arraycopy(temp, 0, input, start, tempIndex);
+    }
+    
+    // Below is how we did it in class
+    public void mergeSort(int[] a, int l, int r)
+    {
+        if (l < r)
+        {
+            int m = (l + r) / 2;                        // Calculating the midpoint
+            
+            mergeSort(a, l, m);
+            mergeSort(a, m + 1, r);                     // Splitting the array
+            
+            merge(a, l, m, r);                          // Merging the arrays
+        }
+    }
+    
+    public void merge(int[] a, int l, int m, int r)
+    {
+        int n1 = m - l + 1;             // This is the size of the left sub array
+        int n2 = r - m;                 // This is the size of the right sub array 
+    
+        // Creating temporary arrays
+        int[] lA = new int[n1];
+        int[] rA = new int[n2];
+       
+        // Copying into the new array
+        for(int i = 0; i < n1; i++)
+        {
+            lA[i] = a[l + i];
+        }
+        
+        for(int i = 0; i < n2; i++)
+        {
+            rA[i] = a[m + 1 + i];
+        }
+        
+        int k = 0, j= 0, c = 0;             // Temporary indices
+        
+        while(k < n1 && j < n2)
+        {
+            if(lA[k] <= rA[j])
+            {
+                a[c + l] = lA[k];
+                k++;
+            }
+            else
+            {
+                a[c + l] = rA[j];
+                j++;
+            }
+            
+            c++;
+        }
+        
+        
+        while(k < n1)
+        {
+            a[c + l] = lA[k];
+            k++;
+            c++;
+        }
+        
+        while(i < n2)
+        {
+            
+        }
     }
 }
