574697e7c65d37c8ef9ba9868207cb29e44eb569
Milos Arsik <marsik@uwo.ca>
2019-02-07 14:30:32 -0500

    restructured

--- Lists/ArrayList/src/milosarsik/lists/Main.java
+++ Lists/ArrayList/src/milosarsik/lists/Main.java
@@ -3,7 +3,8 @@ package milosarsik.lists;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class Main
+{
     public static void main(String[] args)
     {
         List<Employee> employeeList = new ArrayList<>();
